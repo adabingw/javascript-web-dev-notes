2a990f157c22f92ed429b2a4bd478f604a3413c8
Ada W <adabingw@yahoo.com>
2021-12-23 18:51:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // document.body.innerHTML = "W";
-var s = "Hello, I'm Ada!";
-var countdown = s.length;
+var headingEl = document.getElementById("table");
+console.log(headingEl);
+headingEl.innerHTML = "table of continents";
+
+var countdown = 27;
+var s = "javascript web development.";
 // Step 2. What function will change it each time?
 var heading = document.getElementById("title2");
 heading.innerHTML = "";
 heading.style.color = "black";
-heading.style.fontWeight = "bold";
 var i = 0;
 var countItDown = function() {
   if (countdown > 0) {
@@ -18,29 +21,6 @@ var countItDown = function() {
 };
 // Step 3: Call that on an interval
 var timer = window.setInterval(countItDown, 200);
-
-$(document).ready(function() {
-
-/* Every time the window is scrolled ... */
-$(window).scroll( function(){
-
-    /* Check the location of each desired element */
-    $('.hideme').each( function(i){
-
-        var bottom_of_object = $(this).position().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        var offset = $(this).offset();
-
-        /* If the object is completely visible in the window, fade it it */
-        if( bottom_of_window > (bottom_of_object) ){
-
-            $(this).animate({'opacity':'1'},1000);
-
-        }    
-
-    }); 
-}); 
-});
 
 //   var slideShow = function(container) {
 //     this.images = [];
@@ -67,30 +47,30 @@ $(window).scroll( function(){
 // };
 // slideShow(document.getElementById("slideshow"));
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-// }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
